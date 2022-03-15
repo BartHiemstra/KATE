5e85d0ef497a8614bc997a123be44b6b6e3c70b0
BartHiemstra <bart.hiemstra@student.hu.nl>
2022-03-15 16:31:17 +0100

    Changed .env

--- backend/routes/address.js
+++ backend/routes/address.js
@@ -1,16 +1,17 @@
 const axios = require('axios');
 const router = require('express').Router();
+require('dotenv').config();
 
 //GET request: Return building info acquired from Kadaster API based on postal code and house number.
 router.route('/:postalCode/:houseNumber').get((req, res) => {
   axios.get('https://api.bag.kadaster.nl/lvbag/individuelebevragingen/v2/adressenuitgebreid?postcode=' + req.params.postalCode + '&huisnummer=' + req.params.houseNumber + '&exacteMatch=true', {
       headers: {
-        'X-Api-Key': 'l760c9d815d7734995a926ed3303190e50',
+        'X-Api-Key': process.env.API_KADASTER,
         'Accept-Crs': 'epsg:28992'
       }
     })
     .then(response => res.send(response.data))
-    .catch(err => console.log('Error: ' + err));
+    .catch(err => console.log('Error: ' + err)); //TODO: Respond with error code
 });
 
 module.exports = router;
