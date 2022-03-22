5e8ebf0044e0b3cf7ad3afa192025a449f593d43
BartHiemstra <bart.hiemstra@student.hu.nl>
2022-03-22 20:52:33 +0100

    Added basic building info to Properties page

--- frontend/src/components/screens/screen_address/screen_address.component.js
+++ frontend/src/components/screens/screen_address/screen_address.component.js
@@ -19,6 +19,7 @@ export default class ScreenAddress extends Component {
                 postal: '',
                 street: '',
                 number: '',
+                city: '',
                 year: '',
                 surface: ''
             },
@@ -34,28 +35,34 @@ export default class ScreenAddress extends Component {
                     postal: postalCode,
                     street: response.data._embedded.adressen[0].openbareRuimteNaam,
                     number: response.data._embedded.adressen[0].huisnummer,
+                    city: response.data._embedded.adressen[0].woonplaatsNaam,
                     year: response.data._embedded.adressen[0].oorspronkelijkBouwjaar,
                     surface: response.data._embedded.adressen[0].oppervlakte
                 },
             })
+            // On success, set the building info in parent class then call for Properties screen.
             this.props.setBuilding(this.state.building)
             this.props.showComponent('Properties')
         })
         .catch(error => console.log(error));
     }
 
+    // Render Address screen
     render() {
         return (
             <div>
                 <div className='background-img'></div>
                 <div className='container vh-100'>
                     <div className='row semi-center'>
+                        <h3>Materiële restwaarde berekenen</h3>
+                    </div>
+                    <div className='row'>
                         <div className='col'>
                             <Autocomplete
                                 language={'NL'}
                                 options={{ types: ['address'], componentRestrictions: { country: 'nl' } }}
                                 apiKey={ GOOGLE_API_KEY }
-                                placeholder="Straatnaam en huisnummer"
+                                placeholder="Voer een adres in"
                                 onPlaceSelected={(place) => {
                                     // When a place is selected, search for postal code and house number in the address_components array.
                                     let postalCode = null
--- frontend/src/components/screens/screen_properties/screen_properties.component.js
+++ frontend/src/components/screens/screen_properties/screen_properties.component.js
@@ -9,194 +9,62 @@ export default class ScreenProperties extends Component {
   constructor(props) {
     super(props);
 
-    this.onChangeAddressPostal = this.onChangeAddressPostal.bind(this);
-    this.onChangeAddressHouseNum = this.onChangeAddressHouseNum.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.scrollRef = React.createRef()
+    this.onChangeFloors = this.onChangeFloors.bind(this);
 
     this.state = {
-      addressPostal: '',
-      addressHouseNum: '',
-      buildingInfo: '',
-      buildingStreet: '',
-      buildingNumer: '',
-      buildingYear: '',
-      buildingSurface: ''
+      floors: '',
+      foundationPiles: '',
+      foundationBeams: '',
     }
   }
 
-  onChangeAddressPostal(e) {
+  onChangeFloors(e) {
     this.setState({
-      addressPostal: e.target.value
-    })
-  }
-
-  onChangeAddressHouseNum(e) {
-    this.setState({
-      addressHouseNum: e.target.value
-    })
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    axios.get(BASE_URL + 'address/' + this.state.addressPostal + '/' + this.state.addressHouseNum)
-      .then(response => {
-        this.setState({
-          buildingInfo: 'Gebouwgegevens',
-          buildingStreet: response.data._embedded.adressen[0].openbareRuimteNaam,
-          buildingNumber: response.data._embedded.adressen[0].huisnummer,
-          buildingYear: response.data._embedded.adressen[0].oorspronkelijkBouwjaar,
-          buildingSurface: response.data._embedded.adressen[0].oppervlakte + ' m2'
-        })
-        this.scrollRef.current.scrollIntoView()
-      })
-      .catch(error => console.log(error)
-    );
-  }
-
-  handleClick() {
+      floors: e.target.value,
+      foundationPiles: Math.round(this.props.buildingInfo.surface / this.state.floors / 9),
+      foundationBeams: Math.round(this.props.buildingInfo.surface / this.state.floors / 6)
+    })      
   }
 
   render() {
     return (
       <div>
-        <div className='container text-center'>
-          <h5>{this.props.buildingInfo.street} {this.props.buildingInfo.number}</h5>
+        <div className='container vh-100'>
+          <div className='row padding-top-2 text-center'>
+            <h2>{this.props.buildingInfo.street} {this.props.buildingInfo.number}</h2>
+            <h5>{this.props.buildingInfo.postal}, {this.props.buildingInfo.city}</h5>
+          </div>
+          <div className='row padding-top-2'>
+            <div className='col'>
+            <label>Bouwjaar</label>
+                <input readOnly type="text" value={this.props.buildingInfo.year} name="input-year" className="form-control"/>
+            </div>
+          </div>
+          <div className='row padding-top-1'>
+            <div className='col'>
+            <label>Gebruiksoppervlakte (GBO)</label>
+                <input readOnly type="text" value={this.props.buildingInfo.surface + ' m2'} name="input-surface" className="form-control"/>
+            </div>
+          </div>
+          <div className='row padding-top-1'>
+            <div className='col'>
+            <label>Verdiepingen</label>
+                <input type="text" value={this.state.floors} onChange={this.onChangeFloors} name="input-floors" className="form-control"/>
+            </div>
+          </div>
+          <div className='row padding-top-1'>
+            <div className='col'>
+            <label>Fundering palen</label>
+                <input type="text" value={this.state.foundationPiles} name="input-foundationPiles" className="form-control"/>
+            </div>
+          </div>
+          <div className='row padding-top-1'>
+            <div className='col'>
+            <label>Fundering balken</label>
+                <input type="text" value={this.state.foundationBeams} name="input-foundationBeams" className="form-control"/>
+            </div>
+          </div>
         </div>
-      <div className="container">
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-          </div>
-          <br></br>
-          <div className="form-group">
-            <input type="submit" value="VerSDFSDKFJSLDFKJder" className="btn btn-primary" />
-          </div>
-          <br></br>
-          <h5 ref={this.scrollRef}>{this.state.buildingInfo}</h5>
-          <label>{this.state.buildingStreet} {this.state.buildingNumber}</label>
-          <br></br>
-          <label>{this.state.buildingYear}</label>
-          <br></br>
-          <label >{this.state.buildingSurface}</label>
-        </form>
-        
-        <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col'>
-                <label>LALALALDSLSLDFKJ: </label>
-                <input required type="text" name="input-postal" className="form-control" value={this.state.addressPostal} onChange={this.onChangeAddressPostal}/>
-              </div>
-              <div className='col'>
-                <label>HuiSDFS:DLFLSDKJSJLDKFsnummer: </label>
-                <input required type="text" name="input-housenumber" className="form-control" value={this.state.addressHouseNum} onChange={this.onChangeAddressHouseNum}/>
-              </div>
-            </div>
-          </div>
       </div>
     )
   }
